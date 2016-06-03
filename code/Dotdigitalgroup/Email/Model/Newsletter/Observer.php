aa33b2b9eed7442bb093f961d772a298c4eab251
Adeel Qamar <adeel.qamar@dotmailer.com>
2016-06-03 11:33:02 +0200

    Subscriber_updated records filling up email_importer #70

--- code/Dotdigitalgroup/Email/Model/Newsletter/Observer.php
+++ code/Dotdigitalgroup/Email/Model/Newsletter/Observer.php
@@ -3,125 +3,138 @@
 class Dotdigitalgroup_Email_Model_Newsletter_Observer
 {
 
-	/**
-	 * Change the subscribsion for an contact.
-	 * Add new subscribers to an automation.
-	 *
-	 * @param Varien_Event_Observer $observer
-	 *
-	 * @return $this
-	 */
-	public function handleNewsletterSubscriberSave(Varien_Event_Observer $observer)
-	{
-		$subscriber = $observer->getEvent()->getSubscriber();
-		$email              = $subscriber->getEmail();
-		$storeId            = $subscriber->getStoreId();
-		$subscriberStatus   = $subscriber->getSubscriberStatus();
-		$websiteId = Mage::app()->getStore($subscriber->getStoreId())->getWebsiteId();
+    /**
+     * Change the subscribsion for an contact.
+     * Add new subscribers to an automation.
+     *
+     * @param Varien_Event_Observer $observer
+     *
+     * @return $this
+     */
+    public function handleNewsletterSubscriberSave(Varien_Event_Observer $observer)
+    {
+        $subscriber = $observer->getEvent()->getSubscriber();
+        $email = $subscriber->getEmail();
+        $storeId = $subscriber->getStoreId();
+        $subscriberStatus = $subscriber->getSubscriberStatus();
+        $websiteId = Mage::app()->getStore($subscriber->getStoreId())->getWebsiteId();
 
-		//check if enabled
-		if ( ! Mage::helper('ddg')->isEnabled($websiteId))
-			return $this;
-		try{
+        //check if enabled
+        if (!Mage::helper('ddg')->isEnabled($websiteId)) {
+            return $this;
+        }
+        try {
 
-			$contactEmail = Mage::getModel('ddg_automation/contact')->loadByCustomerEmail($email, $websiteId);
+            $contactEmail = Mage::getModel('ddg_automation/contact')->loadByCustomerEmail($email, $websiteId);
 
-			// only for subscribers
-			if ($subscriberStatus == Mage_Newsletter_Model_Subscriber::STATUS_SUBSCRIBED) {
-				//Set contact as subscribed
-				$contactEmail->setSubscriberStatus($subscriberStatus)
-					->setIsSubscriber('1');
+            // only for subscribers
+            if ($subscriberStatus == Mage_Newsletter_Model_Subscriber::STATUS_SUBSCRIBED) {
+                //Set contact as subscribed
+                $contactEmail->setSubscriberStatus($subscriberStatus)
+                    ->setIsSubscriber('1');
 
-				//Subscriber subscribed when it is suppressed in table then re-subscribe
-				if($contactEmail->getSuppressed()){
-					Mage::getModel('ddg_automation/importer')->registerQueue(
-						Dotdigitalgroup_Email_Model_Importer::IMPORT_TYPE_SUBSCRIBER_RESUBSCRIBED,
-						array('email' => $email),
-						Dotdigitalgroup_Email_Model_Importer::MODE_SUBSCRIBER_RESUBSCRIBED,
-						$websiteId
-					);
-					//Set to subscriber imported and reset the subscriber as suppressed
-					$contactEmail->setSubscriberImported(1)
-						->setSuppressed(null);
-				}
+                //Subscriber subscribed when it is suppressed in table then re-subscribe
+                if ($contactEmail->getSuppressed()) {
+                    Mage::getModel('ddg_automation/importer')->registerQueue(
+                        Dotdigitalgroup_Email_Model_Importer::IMPORT_TYPE_SUBSCRIBER_RESUBSCRIBED,
+                        array('email' => $email),
+                        Dotdigitalgroup_Email_Model_Importer::MODE_SUBSCRIBER_RESUBSCRIBED,
+                        $websiteId
+                    );
+                    //Set to subscriber imported and reset the subscriber as suppressed
+                    $contactEmail->setSubscriberImported(1)
+                        ->setSuppressed(null);
+                }
 
-				//not subscribed
-			}else {
-				//skip if contact is suppressed
-				if ($contactEmail->getSuppressed())
-					return $this;
+                //not subscribed
+            } else {
+                $unsubscribeEmails = Mage::registry('unsubscribeEmails');
+                if (is_array($unsubscribeEmails)) {
+                    if (in_array($email, $unsubscribeEmails)) {
+                        return $this;
+                    }
+                }
 
-				//update contact id for the subscriber
-				$contactId = $contactEmail->getContactId();
-				//get the contact id
-				if ( !$contactId ) {
-					Mage::getModel('ddg_automation/importer')->registerQueue(
-						Dotdigitalgroup_Email_Model_Importer::IMPORT_TYPE_SUBSCRIBER_UPDATE,
-						array('email' => $email, 'id' => $contactEmail->getId()),
-						Dotdigitalgroup_Email_Model_Importer::MODE_SUBSCRIBER_UPDATE,
-						$websiteId
-					);
-				}
-				$contactEmail->setIsSubscriber(null)
-					->setSubscriberStatus(Mage_Newsletter_Model_Subscriber::STATUS_UNSUBSCRIBED);
-			}
+                //skip if contact is suppressed
+                if ($contactEmail->getSuppressed()) {
+                    return $this;
+                }
 
-			// fix for a multiple hit of the observer
-			$emailReg =  Mage::registry($email . '_subscriber_save');
-			if ($emailReg){
-				return $this;
-			}
-			Mage::register($email . '_subscriber_save', $email);
+                //update contact id for the subscriber
+                $contactId = $contactEmail->getContactId();
+                //get the contact id
+                if (!$contactId) {
+                    Mage::getModel('ddg_automation/importer')->registerQueue(
+                        Dotdigitalgroup_Email_Model_Importer::IMPORT_TYPE_SUBSCRIBER_UPDATE,
+                        array('email' => $email, 'id' => $contactEmail->getId()),
+                        Dotdigitalgroup_Email_Model_Importer::MODE_SUBSCRIBER_UPDATE,
+                        $websiteId
+                    );
+                }
+                $contactEmail->setIsSubscriber(null)
+                    ->setSubscriberStatus(Mage_Newsletter_Model_Subscriber::STATUS_UNSUBSCRIBED);
+            }
 
-			//add subscriber to automation
-			$this->_addSubscriberToAutomation($email, $subscriber, $websiteId);
+            // fix for a multiple hit of the observer
+            $emailReg = Mage::registry($email . '_subscriber_save');
+            if ($emailReg) {
+                return $this;
+            }
+            Mage::register($email . '_subscriber_save', $email);
 
-			//update the contact
-			$contactEmail->setStoreId($storeId);
+            //add subscriber to automation
+            $this->_addSubscriberToAutomation($email, $subscriber, $websiteId);
 
-			//update contact
-			$contactEmail->save();
+            //update the contact
+            $contactEmail->setStoreId($storeId);
 
-		}catch(Exception $e){
-			Mage::logException($e);
-			Mage::helper('ddg')->getRaygunClient()->SendException($e, array(Mage::getBaseUrl('web')));
-		}
-		return $this;
-	}
+            //update contact
+            $contactEmail->save();
 
-	protected function _addSubscriberToAutomation($email, $subscriber, $websiteId){
+        } catch (Exception $e) {
+            Mage::logException($e);
+            Mage::helper('ddg')->getRaygunClient()->SendException($e, array(Mage::getBaseUrl('web')));
+        }
+        return $this;
+    }
 
-		$storeId            = $subscriber->getStoreId();
-		$store = Mage::app()->getStore($storeId);
-		$programId     = Mage::helper( 'ddg' )->getAutomationIdByType( 'XML_PATH_CONNECTOR_AUTOMATION_STUDIO_SUBSCRIBER', $websiteId );
-		//not mapped ignore
-		if (! $programId)
-			return;
-		try {
-			//check the subscriber alredy exists
-			$enrolmentCollection = Mage::getModel('ddg_automation/automation')->getCollection()
-				->addFieldToFilter('email', $email)
-				->addFieldToFilter('automation_type', Dotdigitalgroup_Email_Model_Automation::AUTOMATION_TYPE_NEW_SUBSCRIBER)
-				->addFieldToFilter('website_id', $websiteId);
-			$enrolmentCollection->getSelect()->limit(1);
-			$enrolment = $enrolmentCollection->getFirstItem();
+    protected function _addSubscriberToAutomation($email, $subscriber, $websiteId)
+    {
 
-			//add new subscriber to automation
-			if (! $enrolment->getId()) {
-				//save subscriber to the queue
-				$automation = Mage::getModel( 'ddg_automation/automation' );
-				$automation->setEmail( $email )
-					->setAutomationType( Dotdigitalgroup_Email_Model_Automation::AUTOMATION_TYPE_NEW_SUBSCRIBER )
-					->setEnrolmentStatus( Dotdigitalgroup_Email_Model_Automation::AUTOMATION_STATUS_PENDING )
-					->setTypeId( $subscriber->getId() )
-					->setWebsiteId( $websiteId )
-					->setStoreName( $store->getName() )
-					->setProgramId( $programId );
-				$automation->save();
-			}
-		}catch(Exception $e){
-			Mage::logException($e);
-		}
-	}
+        $storeId = $subscriber->getStoreId();
+        $store = Mage::app()->getStore($storeId);
+        $programId = Mage::helper('ddg')->getAutomationIdByType('XML_PATH_CONNECTOR_AUTOMATION_STUDIO_SUBSCRIBER',
+            $websiteId);
+        //not mapped ignore
+        if (!$programId) {
+            return;
+        }
+        try {
+            //check the subscriber alredy exists
+            $enrolmentCollection = Mage::getModel('ddg_automation/automation')->getCollection()
+                ->addFieldToFilter('email', $email)
+                ->addFieldToFilter('automation_type',
+                    Dotdigitalgroup_Email_Model_Automation::AUTOMATION_TYPE_NEW_SUBSCRIBER)
+                ->addFieldToFilter('website_id', $websiteId);
+            $enrolmentCollection->getSelect()->limit(1);
+            $enrolment = $enrolmentCollection->getFirstItem();
+
+            //add new subscriber to automation
+            if (!$enrolment->getId()) {
+                //save subscriber to the queue
+                $automation = Mage::getModel('ddg_automation/automation');
+                $automation->setEmail($email)
+                    ->setAutomationType(Dotdigitalgroup_Email_Model_Automation::AUTOMATION_TYPE_NEW_SUBSCRIBER)
+                    ->setEnrolmentStatus(Dotdigitalgroup_Email_Model_Automation::AUTOMATION_STATUS_PENDING)
+                    ->setTypeId($subscriber->getId())
+                    ->setWebsiteId($websiteId)
+                    ->setStoreName($store->getName())
+                    ->setProgramId($programId);
+                $automation->save();
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+    }
 
 }
