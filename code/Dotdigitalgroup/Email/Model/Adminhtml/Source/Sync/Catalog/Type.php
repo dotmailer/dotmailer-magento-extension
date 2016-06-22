102a467b11b1509ceb0eb6a4aa72270092b8646f
Adeel Qamar <adeel.qamar@dotmailer.com>
2016-06-22 09:42:23 +0100

    Add --Default Option-- to multiselect configs #85

--- code/Dotdigitalgroup/Email/Model/Adminhtml/Source/Sync/Catalog/Type.php
+++ code/Dotdigitalgroup/Email/Model/Adminhtml/Source/Sync/Catalog/Type.php
@@ -11,6 +11,7 @@ class Dotdigitalgroup_Email_Model_Adminhtml_Source_Sync_Catalog_Type
     public function toOptionArray()
     {
         $options = Mage::getModel('catalog/product_type')->getAllOptions();
+        //Add default option to first key of array. First key has empty value and empty label.
         $options[0]['label'] = '---- Default Option ----';
         $options[0]['value'] = '0';
         return $options;
