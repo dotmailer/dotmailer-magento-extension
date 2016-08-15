5f152cf78eb3e75b29e94a7fbe250241299a18b6
Adeel Qamar <adeel.qamar@dotmailer.com>
2016-08-15 15:30:38 +0200

    Mark customers as subscribers

--- code/Dotdigitalgroup/Email/sql/email_connector_setup/mysql4-upgrade-6.1.0-6.1.1.php
+++ code/Dotdigitalgroup/Email/sql/email_connector_setup/mysql4-upgrade-6.1.0-6.1.1.php
@@ -20,4 +20,25 @@ $installer->getConnection()->addColumn(
     )
 );
 
+//Update contacts with customers that are subscribers
+$select = $installer->getConnection()->select();
+
+//join
+$select->joinLeft(
+    array('ns' => $installer->getTable('newsletter/subscriber')),
+    "dc.customer_id = ns.customer_id",
+    array(
+        'is_subscriber' => new Zend_Db_Expr('1'),
+        'subscriber_status' => new Zend_Db_Expr('1')
+    )
+)
+    ->where('ns.subscriber_status =?', 1);
+
+//update query from select
+$contactTable = $installer->getTable('ddg_automation/contact');
+$updateSql = $select->crossUpdateFromSelect(array('dc' => $contactTable));
+
+//run query
+$installer->getConnection()->query($updateSql);
+
 $installer->endSetup();
