d44e80048432fe6636c4fcae039f61c6ef77f6b2
Adeel Qamar <adeel.qamar@dotmailer.com>
2017-03-09 15:55:13 +0000

    Show correct website assigned to subscriber #222

--- code/Dotdigitalgroup/Email/Block/Adminhtml/Column/Renderer/Subscriber.php
+++ code/Dotdigitalgroup/Email/Block/Adminhtml/Column/Renderer/Subscriber.php
@@ -15,9 +15,11 @@ class Dotdigitalgroup_Email_Block_Adminhtml_Column_Renderer_Subscriber
     {
         $value = $this->_getValue($row);
         $storeId = $row->getStoreId();
+        $websiteHash = Mage::getSingleton('adminhtml/system_store')
+            ->getWebsiteOptionHash(true);
         if($value == 0 && $storeId > 0) {
-            return Mage::app()->getStore($storeId)->getWebsite()->getId();
+            return $websiteHash[Mage::app()->getStore($storeId)->getWebsite()->getId()];
         }
-        return parent::render($row);
+        return Mage::app()->getWebsite($value)->getName();
     }
 }
