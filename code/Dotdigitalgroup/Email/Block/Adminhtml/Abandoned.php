ad49c926063aa1bae24faad144c0434c4ee67193
Calin Diacon <calin.diacon@dotmailer.com>
2017-09-11 12:35:12 +0100

    fix ajax grid filter

--- code/Dotdigitalgroup/Email/Block/Adminhtml/Abandoned.php
+++ code/Dotdigitalgroup/Email/Block/Adminhtml/Abandoned.php
@@ -9,7 +9,7 @@ class Dotdigitalgroup_Email_Block_Adminhtml_Abandoned
      */
     public function __construct()
     {
-        $this->_controller = 'adminhtml_Abandoned';
+        $this->_controller = 'adminhtml_abandoned';
         $this->_blockGroup = 'ddg_automation';
         parent::__construct();
         $this->_headerText = Mage::helper('ddg')->__('Abandoned Carts');
