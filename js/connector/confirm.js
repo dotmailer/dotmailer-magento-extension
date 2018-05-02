f6b257d5ccdae7e4ee804e287ad264f5cfa505a4
Adeel Qamar <Adeel.Qamar@dotmailer.com>
2018-05-02 09:00:17 +0000

    Merged PR 7469: Update description one for sync and one for generic.
    Update description one for sync and one for generic.

--- js/connector/confirm.js
+++ js/connector/confirm.js
@@ -7,10 +7,18 @@ $j(document).ready(function () {
      * @param {Object} element
      */
     function showConfirmation(element) {
-        var msg = 'You are about to enable this feature for customers that haven\'t explicitly opted into your ' +
-            'emails. Do you wish to continue?';
+        var content;
 
-        if (confirm(msg)) {
+        if (element.attr('id') === 'connector_sync_settings_address_book_allow_non_subscribers') {
+            content = 'You are about to allow dotmailer to import customers that haven\'t explicitly opted into ' +
+                'your emails. This means Customers and Guests address book will contain contacts that you might not ' +
+                'be able to reach out, depending on the applicable regulations. Do you wish to continue?';
+        } else {
+            content = 'You are about to enable this feature for customers that haven\'t explicitly opted into your ' +
+                'emails. Do you wish to continue?';
+        }
+
+        if (confirm(content)) {
             element.val(1);
         } else {
             element.val(0);
