bc8c18307009e4dd0de4a218edce84b8092f6ae6
Adeel Qamar <Adeel.Qamar@dotmailer.com>
2018-06-27 08:45:13 +0000

    Merged PR 8288: Observe events for catalog bulk attribute update #71936
    Reset catalog entries when client does a bulk update on selected product using "update attribute" option from dropdown.
    
    The fix
    Proposed solution in planning was to observe events on M1 where all needed events are available.
    
    Observing following events
    catalog_product_attribute_update_after
    catalog_product_stock_item_mass_change
    catalog_product_to_website_change
    
    Please note this PR is only for bulk operation "update attribute" from Products report.
    
    Why PR produced?
    See attached work item for more info on this.
    
    Steps to Reproduce Bus
    In Magento backend go to Catalog -> Products
    Select product(s). Make sure same products are marked as importer in email_catalog.
    From above drop down choose "Update Attribute"
    Make changes and save.
    Above selected products are not marked not importer in email_catalog
    
    Actual Result
    Bulk update "update attribute" on selected products do not mark already imported items in email_catalog as not imported
    
    Expected Result
    Bulk update "update attribute" on selected products will mark already imported items in email_catalog as not imported
    
    Related work items: #71936

---
+++ code/Dotdigitalgroup/Email/Model/Catalog/Observer.php
@@ -0,0 +1,33 @@
+<?php
+
+class Dotdigitalgroup_Email_Model_Catalog_Observer
+{
+    /**
+     * Update attribute mass action observer for observing attributes, inventory and websites events
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function afterUpdateAttributes(Varien_Event_Observer $observer)
+    {
+        $eventName = $observer->getEvent()->getName();
+
+        switch ($eventName) {
+            case "catalog_product_attribute_update_after":
+                $productIds = $observer->getEvent()->getProductIds();
+                break;
+
+            case "catalog_product_stock_item_mass_change":
+            case "catalog_product_to_website_change":
+                $productIds = $observer->getEvent()->getProducts();
+                break;
+
+            default:
+                $productIds = array();
+                break;
+        }
+
+        if (! empty($productIds)) {
+            Mage::getResourceModel('ddg_automation/catalog')->setModified($productIds);
+        }
+    }
+}
