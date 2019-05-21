78a583139cfb10b28407d1bae87108b028284f0d
Adeel Qamar <Adeel.Qamar@dotmailer.com>
2019-05-21 12:49:36 +0000

    Merged PR 13789: merge changes into one upgrade script
    merge changes into one upgrade script

--- code/Dotdigitalgroup/Email/sql/email_connector_setup/mysql4-upgrade-6.4.14-6.4.15.php
+++ code/Dotdigitalgroup/Email/sql/email_connector_setup/mysql4-upgrade-6.4.14-6.4.15.php
@@ -6,13 +6,25 @@ $installer = $this;
 $installer->startSetup();
 
 $contactTable = $installer->getTable('ddg_automation/contact');
-$table = $installer->getConnection()->addColumn(
-    $contactTable, 'last_subscribed_at', array(
-        'type' => Varien_Db_Ddl_Table::TYPE_TIMESTAMP,
-        'nullable' => true,
-        'default' => null,
-        'comment' => 'Last time contact subscribed',
-    )
-);
+if ($installer->getConnection()->isTableExists($contactTable)) {
+    $installer->getConnection()->addColumn(
+        $contactTable,
+        'last_subscribed_at',
+        array(
+            'type' => Varien_Db_Ddl_Table::TYPE_TIMESTAMP,
+            'nullable' => true,
+            'default' => null,
+            'comment' => 'Last time contact subscribed',
+        )
+    );
+}
+
+$orderTable = $installer->getTable('ddg_automation/order');
+if ($installer->getConnection()->isTableExists($orderTable)) {
+    $installer->getConnection()->addIndex(
+        $orderTable,
+        'IDX_EMAIL_ORDER_ORDER_ID',
+        array('order_id'));
+}
 
 $installer->endSetup();
