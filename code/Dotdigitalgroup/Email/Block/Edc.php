003f6cd9ec0667045ec12b26275b0064214c8b49
Ben Johnson <ben.johnson@dotdigital.com>
2019-06-03 14:37:57 +0000

    Merged PR 14001: Remove repeated method and move into a trait
    The Basket and Edc block classes both had a method `getProductImage()` which duplicated code in each. This has been moved into a trait for code cleanliness.
    
    **To test:**
    
    - Ensure EDC templates still have visible images

--- code/Dotdigitalgroup/Email/Block/Edc.php
+++ code/Dotdigitalgroup/Email/Block/Edc.php
@@ -1,7 +1,10 @@
 <?php
 
+use Dotdigitalgroup_Email_Block_ProductImageTrait as ProductImageTrait;
+
 class Dotdigitalgroup_Email_Block_Edc extends Mage_Core_Block_Template
 {
+    use ProductImageTrait;
 
     /**
      * @var
@@ -523,26 +526,5 @@ class Dotdigitalgroup_Email_Block_Edc extends Mage_Core_Block_Template
         //@codingStandardsIgnoreEnd
 
         return $productsToDisplay;
-    }
-
-    /**
-     * @param $product Mage_Catalog_Model_Product
-     * @return Mage_Catalog_Helper_Image
-     */
-    public function getProductImage($product)
-    {
-        $helper = Mage::helper('ddg');
-        if ($helper->getWebsiteConfig(Dotdigitalgroup_Email_Helper_Config::XML_PATH_CONNECTOR_DYNAMIC_PRODUCT_IMAGE)
-            && $product->getTypeId() == "simple"
-        ) {
-            $parentIds = Mage::getModel('catalog/product_type_configurable')->getParentIdsByChild($product->getId());
-            if (!empty($parentIds)) {
-                /** @var Mage_Catalog_Model_Product $parentProduct */
-                $parentProduct = Mage::getModel('catalog/product')->load($parentIds[0]);
-                return $this->helper('catalog/image')->init($parentProduct, 'small_image')->resize(135);
-            }
-        }
-
-        return $this->helper('catalog/image')->init($product, 'small_image')->resize(135);
     }
 }
