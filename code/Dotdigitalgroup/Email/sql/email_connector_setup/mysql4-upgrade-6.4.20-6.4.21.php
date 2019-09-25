623434ecbcf4d539443f2a8d7b532fc33ab73b78
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2019-09-25 13:57:22 +0000

    Merged PR 16402: Hotfix: catalog upgrade schema
    To ensure matching columns and key names after upgrade.

--- code/Dotdigitalgroup/Email/sql/email_connector_setup/mysql4-upgrade-6.4.20-6.4.21.php
+++ code/Dotdigitalgroup/Email/sql/email_connector_setup/mysql4-upgrade-6.4.20-6.4.21.php
@@ -8,37 +8,44 @@ $installer->startSetup();
 $catalogTable = $installer->getTable('ddg_automation/catalog');
 
 if ($installer->getConnection()->isTableExists($catalogTable)) {
-    $installer->getConnection()->changeColumn($catalogTable, 'modified','last_imported_at',array(
+
+    if ($installer->getConnection()->tableColumnExists($catalogTable, 'modified')) {
+
+        $installer->getConnection()->dropIndex(
+            $installer->getTable($catalogTable),
+            'IDX_EMAIL_CATALOG_MODIFIED'
+        );
+
+        $installer->getConnection()->changeColumn($catalogTable, 'modified', 'last_imported_at', array(
             'type' => Varien_Db_Ddl_Table::TYPE_TIMESTAMP,
             'nullable' => true,
             'default' => null,
             'comment' => 'Last imported date',
         ));
 
-    $installer->getConnection()->dropIndex(
-        $installer->getTable($catalogTable),
-        'IDX_EMAIL_CATALOG_IMPORTED'
-    );
+        $installer->getConnection()->addIndex($catalogTable,
+            $installer->getIdxName($catalogTable, array('last_imported_at')),
+            array('last_imported_at')
+        );
+    }
 
-    $installer->getConnection()->dropIndex(
-        $installer->getTable($catalogTable),
-        'IDX_EMAIL_CATALOG_MODIFIED'
-    );
+    if ($installer->getConnection()->tableColumnExists($catalogTable, 'imported')) {
+        $installer->getConnection()->dropIndex(
+            $installer->getTable($catalogTable),
+            'IDX_EMAIL_CATALOG_IMPORTED'
+        );
 
-    $installer->getConnection()->changeColumn($catalogTable, 'imported', 'processed', array(
-        'type' => Varien_Db_Ddl_Table::TYPE_SMALLINT,
-        'unsigned' => true,
-        'nullable' => false,
-        'comment' => 'Product processed'
-    ));
+        $installer->getConnection()->changeColumn($catalogTable, 'imported', 'processed', array(
+            'type' => Varien_Db_Ddl_Table::TYPE_SMALLINT,
+            'unsigned' => true,
+            'nullable' => false,
+            'default' => 0,
+            'comment' => 'Product processed'
+        ));
 
-    $installer->getConnection()->addIndex($catalogTable,
-        $installer->getIdxName($catalogTable, array('processed')),
-        array('processed')
-    );
-
-    $installer->getConnection()->addIndex($catalogTable,
-        $installer->getIdxName($catalogTable, array('last_imported_at')),
-        array('last_imported_at')
-    );
+        $installer->getConnection()->addIndex($catalogTable,
+            $installer->getIdxName($catalogTable, array('processed')),
+            array('processed')
+        );
+    }
 }
